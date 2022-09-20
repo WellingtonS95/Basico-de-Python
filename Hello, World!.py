e82332e4c997a2a5396bd9d1889a94c5eae2ce6e
Wellington Barboza <111764526+WellingtonS95@users.noreply.github.com>
2022-09-20 20:02:09 -0300

    Primeiros passos de todo Programador

---
+++ Hello, World!.py
@@ -0,0 +1,12 @@
+print('Hello World!')
+
+#Se você está começando assim como eu, nesse momento, eu te desejo toda sorte do mundo, não desanime e bons estudos. Começe com um "Olá, Mundo!" para tirar todo azar. 
+#Usamos essa cerquilha (#) para poder comentar no programa sem ser levado em conta.
+
+#Usamos print para imprimirmos a informação na tela;
+# "A função print() é uma das funções mais importantes e usadas na linguagem Python. Sua função é, basicamente, exibir mensagens na tela ou enviá-las para outro dispositivo, como imprimir dentro de arquivos de texto."//www.bosontreinamentos.com.br
+
+print('Serei um ótimo programador')
+print('Você pode usar uma aspas simples ou dupla, desde que a do final seja igual a do inicio')
+print(" O print não deve ser escrito com 'p' maiúsculo.")
+print('Procure por vídeo aulas, forums, cursos, e fique de olho que sempre existem atualizações.')
