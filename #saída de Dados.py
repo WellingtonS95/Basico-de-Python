2d8d946acbec7e7d5b328574e642e4b16228abe7
Wellington Barboza <111764526+WellingtonS95@users.noreply.github.com>
2022-09-20 20:29:26 -0300

    Usando input e somando 2 números.

---
+++ #saída de Dados.py
@@ -0,0 +1,9 @@
+#saída de Dados
+#Usaremos a função 'int' que converte uma string em um numero inteiro.
+# 'input' que do inglês literalmente é "entrada". Auxilia o usuário imprimindo um dado na tela informando que tipo de dado o programa está aguardando receber.
+#Usaremos uma variável qualquer, exemplo 'n', para numeros.
+
+n = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+s = n + n2            #Usamos esse "s" para efetuar a soma entre o n, e n2
+print('A soma é',s)
